750d57dff44f3d32f5f3fc31d2461c2e9cd95eb2
Olin <cuijianjoy@126.com>
2022-07-04 01:10:22 +0800

    [AIDL] add log for RemoteService
    Signed-off-by: Olin <cuijianjoy@126.com>

--- AIDL/app/src/main/java/com/example/aidl/RemoteService.java
+++ AIDL/app/src/main/java/com/example/aidl/RemoteService.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.os.Process;
 import android.os.RemoteException;
+import android.util.Log;
 
 public class RemoteService extends Service {
+    private final static String TAG = RemoteService.class.getSimpleName();
     private final IRemoteService.Stub binder = new IRemoteService.Stub() {
         @Override
         public int getPid() throws RemoteException {
@@ -27,4 +29,33 @@ public class RemoteService extends Service {
         return binder;
     }
 
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate");
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand intent=" + intent + " flags=" + flags + " startId=" + startId);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "onUnbind intent=" + intent);
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        Log.d(TAG, "onRebind intent=" + intent);
+        super.onRebind(intent);
+    }
 }
